d121ee54f1f237f4ec960a333b8920bad8d479a0
bareket <bareketd1@mail.tau.ac.il>
2025-04-16 17:22:11 +0300

    q1

--- hw2_q1.py
+++ hw2_q1.py
@@ -37,3 +37,25 @@ def english_to_morse(
         Name of output file containing the translated Morse code. Please don't change
         it since it's also hard-coded in the tests file.
     """
+    # Read the text and convert to uppercase
+    with open(input_file, 'r') as f:
+        text = f.read().upper()
+
+    # Convert each character to Morse code using the MORSE_CODE dictionary
+    table = str.maketrans(MORSE_CODE) # Create a translation table
+
+    # splitting the text to lines and mapping each word in this line and join by ""
+    # and then join by new line
+    morse_code = "\n".join(
+        "\n".join(word.translate(table) for word in line.split()) if line.split() else ""
+        for line in text.splitlines()
+    )
+
+    # Write the Morse code to the output file
+    with open(output_file, 'w') as f:
+        f.write(morse_code)
+
+
+if __name__ == "__main__":
+    english_to_morse()
+    print("Conversion completed successfully.")
